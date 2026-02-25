0f52cdd73ed2c811e33b07f0504dd6e1b9352aed
LiamRolf <rolffarms07@gmail.com>
2026-02-25 11:04:13 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,10 @@
+//Liam - All About Me 2/25/26
+
+let myName = "Liam";
+let favColor = "pink";
+let hobby = "playing games";
+let favFood = "grilled salmon";
+
+// Display info here!!!
+
+console.log(`My name is ${myName},`, `My favorite color is ${favColor},`, `My favorite hobby is ${hobby},` , "One of my favorite foods is", favFood);
