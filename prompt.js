a8b92d2f2feb1373aa27357e0ae728eceaec9469
LiamRolf <rolffarms07@gmail.com>
2026-03-04 10:49:32 -0600

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,9 @@
+//Liam Rolf alert, prompt, confirn
+
+//Create prompt to ask the person's name
+let FName = prompt("Enter your name:");
+let age = prompt("Enter your age:,25")
+let oldAge = parseInt(age) + 20;
+
+console.log(`Hello ${FName}, you are ${age} years old!`)
+console.log(`In 20 years, you'll be ${oldAge} years old, geezer.`)
